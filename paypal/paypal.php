9218a96baf316b58fcfbe5e4f9688430e6bff43d
Amine <abenslama2@gmail.com>
2021-07-01 21:46:07 +0200

    useless file

--- paypal/paypal.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'id' => 'AYFWmu-tOr8o0gmduiY6a9B8WkslI9ogGOWrV42v69RjswVODHRZ3x2githEd1RTTXlMhRbtxOxSGfzz',
-    'secret' => 'EP2wxFd6mppGmpN1vrm-NcbHoyrXUP0CYLcZn-U1xg9WpfTJbR3vwe3x9BVxFoqo_TXc-6rBF48nej9D'
-]
-
-?>
